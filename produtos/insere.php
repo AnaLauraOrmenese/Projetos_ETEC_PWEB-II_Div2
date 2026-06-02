486b5f6b43c98ea1b190a95a5da0cbb8875b685c
AnaLauraOrmenese <lalalrmenese@gmail.com>
2026-06-02 12:26:31 -0300

    aula 2/6

--- produtos/insere.php
+++ produtos/insere.php
@@ -25,11 +25,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 
 <body>
-    <div class="container">
+    <div class="container mt-3">
         <form action="" method="post">
             <div class="form-group">
-                <label for="edtnome">NOME</label>
+                <label for="edtnome">Nome</label>
                 <input type="text" name="edtnome" id="edtnome" class="form-control">
+            </div>
+             <div class="form-group">
+                <label for="edtdescricao">Descrição</label>
+                <input type="text" name="edtdescricao" id="edtdescricao" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="edtvlcusto">Valor de Custo</label>
+                <input type="text" name="edtvlcusto" id="edtvlcusto" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="edtvlvenda">Valor de Venda</label>
+                <input type="text" name="edtvlvenda" id="edtvlvenda" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="edtquantidade">Quantidade</label>
+                <input type="number" name="edtquantidade" id="edtquantidade" class="form-control">
+            </div>
+               <div class="form-group">
+                <label for="edtsubcategoria">Subcategoria</label>
+                <input type="text" name="edtsubid" id="edtsubid" class="form-control mb-2"readonly>
+                 <input type="text" name="edtsubcategoria" id="edtsubcategoria" class="form-control">
             </div>
         </form>
     </div>
